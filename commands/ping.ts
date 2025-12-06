fd2d17e479ec8bfe426df5c9a6ed3ee616b36839
Danny Kim <0916dhkim@gmail.com>
2025-12-06 18:11:29 -0500

    Ping pong

--- commands/ping.ts
+++ commands/ping.ts
@@ -1,10 +1,9 @@
 import { SlashCommandBuilder } from "discord.js";
 
-export default {
-  data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with Pong!"),
-  async execute(interaction: any) {
-    await interaction.reply("Pong!");
-  },
-};
+export const data = new SlashCommandBuilder()
+  .setName("ping")
+  .setDescription("Replies with Pong!");
+
+export async function execute(interaction: any) {
+  await interaction.reply("Pong!");
+}
--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import dotenv from "dotenv";
-import fs from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 // Require the necessary discord.js classes
-import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  MessageFlags,
+} from "discord.js";
+import { commandFullPath } from "./collect-commands.js";
 dotenv.config();
 const token = process.env.DISCORD_TOKEN;
 
@@ -25,25 +25,45 @@ client.once(Events.ClientReady, (readyClient) => {
 // @ts-expect-error
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, "commands");
-const commandFiles = fs.readdirSync(foldersPath);
-
-for (const commandFile of commandFiles) {
-  const filePath = path.join(foldersPath, commandFile);
-  const command = await import(filePath);
+for (const commandFile of commandFullPath) {
+  const command = await import(commandFile);
   // Set a new item in the Collection with the key as the command name and the value as the exported module
   if ("data" in command && "execute" in command) {
     // @ts-expect-error
     client.commands.set(command.data.name, command);
   } else {
     console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      `[WARNING] The command at ${commandFile} is missing a required "data" or "execute" property.`
     );
   }
 }
 
-client.on(Events.InteractionCreate, (interaction) => {
-  console.log(interaction);
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
 });
 
 // Log in to Discord with your client's token
