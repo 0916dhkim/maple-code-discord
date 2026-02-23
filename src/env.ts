a0f53db3f0da3070621378186dd6a3b48603a6ce
Harish Kukreja <harish.kukreja@gmail.com>
2026-02-22 21:03:26 -0500

    chore: make error messages more descriptive

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const envSchema = z.object({
-  DISCORD_TOKEN: z.string(),
+  DISCORD_TOKEN: z.string().min(1, "DISCORD_TOKEN is not set"),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_GUILD_ID: z.string(),
   DATABASE_URL: z.string(),
