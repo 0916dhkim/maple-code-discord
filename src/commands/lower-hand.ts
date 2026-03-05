8b141b1c8cbf2a524cd3f941033a44ac909b31f6
Danny Kim <0916dhkim@gmail.com>
2026-03-04 19:20:03 -0500

    chore: format command files

--- src/commands/lower-hand.ts
+++ src/commands/lower-hand.ts
@@ -1,13 +1,13 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandSpec } from '../command.js';
-import { removeFromQueue } from '../services/raisedHandsService.js';
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandSpec } from "../command.js";
+import { removeFromQueue } from "../services/raisedHandsService.js";
 
 export const lowerHand: CommandSpec = {
-    metadata: new SlashCommandBuilder()
-        .setName('lower-hand')
-        .setDescription('Lower your hand to be removed from the queue'),
-    run: async (interaction) => {
-        const result = await removeFromQueue(interaction.user.id);
-        await interaction.reply(result);
-    },
+  metadata: new SlashCommandBuilder()
+    .setName("lower-hand")
+    .setDescription("Lower your hand to be removed from the queue"),
+  run: async (interaction) => {
+    const result = await removeFromQueue(interaction.user.id);
+    await interaction.reply(result);
+  },
 };
