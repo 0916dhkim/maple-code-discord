6f4365e94dcf1e79625cc7ab401d35e1334c2c22
Danny Kim <0916dhkim@gmail.com>
2026-03-21 18:19:28 -0400

    debug

--- src/client.ts
+++ src/client.ts
@@ -2,49 +2,66 @@ import { DiscordSDK } from "@discord/embedded-app-sdk";
 
 const id = import.meta.env.VITE_DISCORD_CLIENT_ID;
 
-if (id == null) {
-  throw new Error("Client ID required");
+function print(content: string) {
+  const output = document.createElement("p");
+  output.innerHTML = content;
+  document.body.appendChild(output);
 }
 
-const discordSdk = new DiscordSDK(id);
+async function everything() {
+  try {
+    if (id == null) {
+      throw new Error("Client ID required");
+    }
+    const discordSdk = new DiscordSDK(id);
+    await discordSdk.ready();
 
-await discordSdk.ready();
+    print("sdk ready");
 
-// Authorize and authenticate
-const { code } = await discordSdk.commands.authorize({
-  client_id: id,
-  response_type: "code",
-  state: "",
-  prompt: "none",
-  scope: ["identify", "guilds"],
-});
+    // Authorize and authenticate
+    const { code } = await discordSdk.commands.authorize({
+      client_id: id,
+      response_type: "code",
+      state: "",
+      prompt: "none",
+      scope: ["identify", "guilds"],
+    });
 
-// 2. Exchange code for token (via your backend)
-const res = await fetch("/api/token", {
-  method: "POST",
-  body: JSON.stringify({ code }),
-});
+    print("code ready");
 
-const body = await res.json();
+    // 2. Exchange code for token (via your backend)
+    const res = await fetch("/api/token", {
+      method: "POST",
+      body: JSON.stringify({ code }),
+    });
 
-if (
-  typeof body !== "object" ||
-  body == null ||
-  !("access_token" in body) ||
-  typeof body.access_token != "string"
-) {
-  throw new Error("Invalid body");
+    print("token ready");
+
+    const body = await res.json();
+
+    if (
+      typeof body !== "object" ||
+      body == null ||
+      !("access_token" in body) ||
+      typeof body.access_token != "string"
+    ) {
+      throw new Error("Invalid body");
+    }
+
+    const { access_token } = body;
+
+    // 3. Authenticate with the SDK
+    const auth = await discordSdk.commands.authenticate({ access_token });
+
+    // ✅ User ID is here
+    // console.log(auth.user.id); // e.g. "123456789012345678"
+    // console.log(auth.user.username); // e.g. "serj"
+    // console.log(auth.user.avatar); // avatar hash
+    print(`${auth.user.username}`);
+  } catch (e) {
+    print(String(e));
+  }
 }
 
-const { access_token } = body;
-
-// 3. Authenticate with the SDK
-const auth = await discordSdk.commands.authenticate({ access_token });
-
-// ✅ User ID is here
-// console.log(auth.user.id); // e.g. "123456789012345678"
-// console.log(auth.user.username); // e.g. "serj"
-// console.log(auth.user.avatar); // avatar hash
-const output = document.createElement("p");
-output.innerHTML = `${auth.user.id} ${auth.user.username}`;
-document.body.appendChild(output);
+const button = document.getElementById("everything");
+button?.addEventListener("click", everything);
