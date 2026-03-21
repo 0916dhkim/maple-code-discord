f063c6623be5ad4474cdfd6565c8501bfe733af7
Danny Kim <0916dhkim@gmail.com>
2026-03-21 16:58:16 -0400

    Add express yolo

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { createClient } from "./create-client.js";
 import { deployCommands } from "./deploy-commands.js";
 import { env } from "./env.js";
+import express from "express";
 
-async function main() {
+async function startBot() {
   await deployCommands();
 
   const client = await createClient();
   client.login(env.DISCORD_TOKEN);
 }
 
-await main();
+await startBot();
+
+const app = express();
+
+app.get("*", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running...");
+});
